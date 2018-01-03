dcee54422a3ca3aed53d002d979073bcf26366d8
SamuelDR <samueldelgadorios@gmail.com>
2018-01-03 02:43:32 -0600

    removeClass

---
+++ index11/app.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+  
+    $("#B1").click(function(){
+        $("p").removeClass("nuevaClase");
+    });
+
+    $("#add").click(function(){
+      $("#caja").addClass("miClase");
+    });
+
+    $("#remove").click(function(){
+      $("#caja").removeClass("miClase");
+    });
+
+
+});
