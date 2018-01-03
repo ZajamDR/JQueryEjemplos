2020b38e0312771d654ca1c3baafcc89b9894e1d
SamuelDR <samueldelgadorios@gmail.com>
2018-01-03 03:07:52 -0600

    Evento-dblclick

---
+++ index13/app.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+
+  $("#p1").dblclick(function(){
+    $(this).hide();
+  });
+
+  $("#p2").dblclick(function(){
+    alert("mensaje");
+  });
+
+  $("#p3").dblclick(function(){
+    $(this).hide();
+  });
+
+});
