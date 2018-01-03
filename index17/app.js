6eb83c10c7b6ff6a848a1bec5bbab76c5903f24b
SamuelDR <samueldelgadorios@gmail.com>
2018-01-03 04:37:25 -0600

    Evento MouseDown

---
+++ index17/app.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+
+  $("#p1").mousedown(function(){
+    $("#p1").css("background-color","blue");
+  });
+
+  $("#p2").mousedown(function(){
+    $("#p2").css("background-color","green");
+  });
+
+});
