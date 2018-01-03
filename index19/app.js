2c872ee4190133a03883c49899577ebeb74aa2dc
SamuelDR <samueldelgadorios@gmail.com>
2018-01-03 15:05:18 -0600

    Evento - mouseover

---
+++ index19/app.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+
+  $("#p1").mouseover(function(){
+    $(this).hide();
+  });
+
+  $("#p2").mouseover(function(){
+    $(this).hide();
+  });
+
+  $("#p3").mouseover(function(){
+    $(this).hide();
+  });
+
+  $("#p4").mouseover(function(){
+    $(this).hide();
+  });
+
+});
