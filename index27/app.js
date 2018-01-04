0b1aa55e52d382d9e8c7a9ff5d8930dd5e25b1b9
SamuelDR <samueldelgadorios@gmail.com>
2018-01-04 01:20:43 -0600

    Contains

---
+++ index27/app.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+
+    //se pondra en color verde todo lo que tenga la palabra programacion  dentro del contenido de los div
+    $("div:contains('programacion')").css("background-color", "green");
+
+});
